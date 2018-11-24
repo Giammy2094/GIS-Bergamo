13439c9185744e42859bdd6c6a5a18101b4ee0a2
Giammy2094 <42800762+Giammy2094@users.noreply.github.com>
2018-11-24 20:07:30 +0100

    py
    fixed dot over bridge coordinates

--- GIS.py
+++ GIS.py
@@ -4,6 +4,7 @@ import os
 import geopandas as gpd
 import pysal as ps
 import pandas as pd
+from shapely.geometry import Point
 
 #change directory
 os.chdir('C:/Users/gianmarco/Desktop/UNI/Management Engineering/Transportation Economics Management/Project/shape')
@@ -83,12 +84,19 @@ rail_bounds
 
 rail = rail[((rail_bounds['minx'] < 11) & (rail_bounds['maxx'] > 9) & (rail_bounds['miny'] < 46.2) & (rail_bounds['maxy'] > 45.3))]
 rail = rail[rail['id'] != 'Water bodies']
+rail.head()
 
+bridge_point = Point(9.692472, 45.693500)
+bridge_point_series = gpd.GeoSeries([bridge_point])
+bridge_bounds = bridge_point_series.bounds
+bridge_bounds
+bridge_point_series = bridge_point_series[((bridge_bounds['minx'] < 11) & (bridge_bounds['maxx'] > 9) & (bridge_bounds['miny'] < 46.2) & (bridge_bounds['maxy'] > 45.3))]
 #plot rail, road and water on province
-fig, ax = plt.subplots(figsize=(100,100), subplot_kw={'aspect':'equal'})
-roads.plot(ax=ax, color='black', linewidth=0.4)
-rail.plot(ax=ax, color='red',linewidth=0.6)
-water.plot(ax=ax, color='blue')
+fig, ax = plt.subplots(figsize=(110,110), subplot_kw={'aspect':'equal'})
 province.plot(column='POLYGON_NM', ax=ax)
+roads.plot(ax=ax, color='black', linewidth=0.4)
+rail.plot(ax=ax, color='orange',linewidth=0.6)
+bridge_point_series.plot(ax=ax, color='red')
+water.plot(ax=ax, color='blue')
 ax.set_xlim(9.39, 10.27)
 ax.set_ylim(45.4, 46.1)
